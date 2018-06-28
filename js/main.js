a1e26c8b2cbb4b50642efff4336514a4fa69c59a
Madeleine Marcano <madeleine1302@gmail.com>
2018-06-27 20:48:59 -0400

    creando fetch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-/* Holiii acá va tu código también */
+const inputText = document.querySelector("input")
+const containerSaldo = document.getElementById("saldo")
+document.getElementById('btnConsultarSaldo').addEventListener('click', cargarAPI)
+
+
+// inputText.addEventListener("keypress", () => {
+//             let key = event.which || event.keyCode;
+//             if (key === 13) { // 13 es valido para enter 
+//                 let bip = inputText.value;
+//                 console.log(bip)
+//                 inputText.value = "";
+//             })
+
+function cargarAPI() {
+    const numberCardBip = document.getElementById('numberCard').value
+    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${numberCardBip}`)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            console.log(data)
+            renderInfo(data)
+        })
+        .catch(error => console.log(error))
+};
+
+const renderInfo = data => {
+// const cambio = Object.entries(data)
+containerSaldo.innerHTML = data.['Saldo tarjeta'];
+});
+}
